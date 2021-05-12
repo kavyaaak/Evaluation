ff8a5c1e3f28f90efe7e74f8ee00ff722a5ac63c
kavya.k <kavya.k@marketsimplified.com>
2021-05-12 10:53:43 +0530

    tasks

--- src/components/screens/register.js
+++ src/components/screens/register.js
@@ -68,9 +68,9 @@ const RegisterScreen = (props) => {
 
     function onChangeEmail(text) {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (re.test(String(email).toLowerCase())) {
+        if (re.test(String(email).toLowerCase()) && re.test(String(email).toUpperCase() )) {
             const foundUser = user.filter(item => {
-                return text == item.email
+                return String(text).toLowerCase() == item.email
             });
             if(foundUser.length >0){
                     setValidEmail(true)
